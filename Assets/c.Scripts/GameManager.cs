9a6ff8bcf52fda4ec662086f431a86a4750c5345
choi juno <hacke11233@gmail.com>
2016-07-29 11:45:03 +0900

    no message

--- Assets/c.Scripts/GameManager.cs
+++ Assets/c.Scripts/GameManager.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-	
 
 	// end game Check
 	public static int gameSet; //0:play 1:win 2:lose 3:wait
 
 	public static int TestNum;
 
+	public static bool tiltCheck;
+	public GameObject tiltOn;
+	public GameObject tiltOff;
 	void Start(){
 		
 
@@ -16,6 +18,12 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void Awake () {
+		if (Application.loadedLevelName == "TestGame") {
+			if (tiltCheck) {
+				tiltOn.SetActive (true);
+				tiltOff.SetActive (false);
+			}
+		}
 		gameSet = 3;
 	}
 
--- Assets/c.Scripts/TestScipts/TestBtn.cs
+++ Assets/c.Scripts/TestScipts/TestBtn.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class TestBtn : MonoBehaviour {
+	public GameObject OpenPanel;
+	public GameObject ClosePanel;
+
 
 	public void BtnClick(){
 		GameManager.TestNum = int.Parse(this.name.Substring (0, 3));
@@ -11,5 +14,28 @@ public class TestBtn : MonoBehaviour {
 
 	public void TestHome() {
 		Application.LoadLevel ("TestMain");
+	}
+
+	public void TestOn(){
+		GameManager.tiltCheck = false;
+		Debug.Log (GameManager.tiltCheck);
+		openP ();
+		closeP ();
+	}
+
+	public void TestOff(){
+		GameManager.tiltCheck = true;
+		Debug.Log (GameManager.tiltCheck);
+		openP ();
+		closeP ();
+	}
+
+	void openP(){
+		OpenPanel.SetActive (true);
+	}
+
+	void closeP(){
+		ClosePanel.SetActive (false);
+
 	}
 }
