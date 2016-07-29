f8add3f71048392c54898993184841107cebee75
LimSihwan <tlghks1009@naver.com>
2016-07-29 17:25:56 +0900

    Test

---
+++ Assets/c.Scripts/LimTestClass.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimTestClass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GoogleManager.GetInstance.InitializeGPGS();
+        if(GoogleManager.GetInstance.bLogin == false)
+        {
+            GoogleManager.GetInstance.LoginGPGS();
+        }
+        else
+        {
+            GoogleManager.GetInstance.LogoutGPGS();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
