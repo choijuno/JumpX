48ea1ca9e52bb4c6dc007c8018b7f6e9436cc70d
LimSihwan <tlghks1009@naver.com>
2016-08-01 16:24:49 +0900

    no message

--- Assets/c.Scripts/LimTestClass.cs
+++ Assets/c.Scripts/LimTestClass.cs
@@ -1,23 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LimTestClass : MonoBehaviour {
 
+    Button GoogleLoginBtn;
+    Text LoginCallBack;
+    Text LogInOut;
 	// Use this for initialization
 	void Start () {
+
+        GoogleLoginBtn = GameObject.Find("GoogleLoginBtn").GetComponent<Button>();
+        GoogleLoginBtn.onClick.AddListener(GoogleLoginBtnFunc);
+
+        LoginCallBack = GameObject.Find("LoginCallBack").GetComponent<Text>();
+        LogInOut = GameObject.Find("LogInOut").GetComponent<Text>();
+
         GoogleManager.GetInstance.InitializeGPGS();
-        if(GoogleManager.GetInstance.bLogin == false)
+        LoginCallBack.text = "로그인 : " + GoogleManager.GetInstance.bLogin.ToString();
+    }
+    void Update()
+    {
+        LoginCallBack.text = "로그인 : " + GoogleManager.GetInstance.bLogin.ToString();
+    }
+	void GoogleLoginBtnFunc()
+    {
+        if (GoogleManager.GetInstance.bLogin == false)
         {
             GoogleManager.GetInstance.LoginGPGS();
+            LogInOut.text = "로그아웃";
+            Debug.Log("로그아웃");
         }
         else
         {
             GoogleManager.GetInstance.LogoutGPGS();
+            LogInOut.text = "로그인";
+            Debug.Log("로그인");
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
 }
