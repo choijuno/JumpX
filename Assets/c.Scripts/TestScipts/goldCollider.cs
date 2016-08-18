fd1fcf38644683726aec89e187dbac10aea34bbf
choi juno <hacke11233@gmail.com>
2016-08-18 16:05:38 +0900

    no message

--- Assets/c.Scripts/TestScipts/goldCollider.cs
+++ Assets/c.Scripts/TestScipts/goldCollider.cs
@@ -5,11 +5,12 @@ public class goldCollider : MonoBehaviour {
 
 	public GameObject model;
 	public GameObject modelDestroy;
+	public AudioClip goldSound;
 
 
 	void OnTriggerEnter(Collider player){
 		if (player.CompareTag ("player")) {
-
+			AudioSource.PlayClipAtPoint(goldSound, new Vector3(this.transform.position.x + 4f, this.transform.position.y, -10f));
 			model.SetActive (false);
 			modelDestroy.SetActive (true);
 			this.GetComponent<BoxCollider> ().enabled = false;
